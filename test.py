8c6eb5837bf09e535e50ac05859a828bfe74d0f4
xenahort <juan.carlos.wow.95@gmail.com>
2018-11-05 11:42:21 +0100

    Prueba de test

---
+++ test.py
@@ -0,0 +1,11 @@
+import unittest
+import app
+
+class TestMyServer(unittest.TestCase):
+
+    def test_first(self):
+        response = app.index()
+        self.assertIn('OK', response, "OK was not found in response")
+
+if __name__ == '__main__':
+    unittest.main()
