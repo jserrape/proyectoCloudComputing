7606e033758a47e9dc3ee67734932bf9f1522135
xenahort <juan.carlos.wow.95@gmail.com>
2018-11-05 14:00:23 +0100

    Revert

--- test.py
+++ test.py
@@ -8,9 +8,5 @@ class TestMyServer(unittest.TestCase):
         response = app.index()
         self.assertIn('OK', response, "Se ha encontrado un error en index")
 
-    def test_secondt(self):
-        response = app.about()
-        self.assertIn('OKa', response, "Se ha encontrado un error en about")
-
 if __name__ == '__main__':
     unittest.main()
