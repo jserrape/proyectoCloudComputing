c1d52685f0ad91a68eb2d843de3ce1b42c9f830d
xenahort <juan.carlos.wow.95@gmail.com>
2018-11-09 12:27:11 +0100

    Eliminados documentos antiguos

--- forms.py
+++
@@ -1,6 +0,0 @@
-from wtforms import Form
-from wtforms.fields import StringField
-from wtforms.widgets import TextArea
-
-class FormularioComentario(Form):
-    comment = StringField(u'Comment', widget=TextArea())
